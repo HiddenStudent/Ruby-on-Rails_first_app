dc45147ed5dc8976f8e3e74ea640f201fc88cac4
oleshko <evgeniy-oleshko@lenta.ru>
2018-11-22 13:56:21 +0200

    model - relationship

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@ User.create!(name:  "Lilya",
              activated: true,
              activated_at: Time.zone.now)
 
-10.times do |n|
+20.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
@@ -23,4 +23,13 @@ User.create!(name:  "Lilya",
     users.each { |user| user.microposts.create!(content: content) }
   end
 
+
+
 end
+# Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
